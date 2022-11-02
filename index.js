8053ec07d326a96f471a24662b0d66734fe3c0b1
Your Name <you@example.com>
2022-11-03 01:41:24 +0600

    make a user admin and delete a admin with jwt validation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken')
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
@@ -16,10 +17,27 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
     const playerCollection = client.db("fifa-2023").collection("players");
+    const productCollection = client.db("fifa-2023").collection("products");
+    const userCollection = client.db("fifa-2023").collection("users");
     console.log("connection succefully");
 
     // get all the player info
@@ -29,11 +47,59 @@ async function run() {
     });
 
     // get a specific player info
-    app.get('/player/:id', async(req, res) =>{
+    app.get("/player/:id", async(req, res) =>{
+      const id = req.params.id;
+      // console.log(id)
+      const filter = {_id: ObjectId(id)}
+      const result = await playerCollection.findOne(filter);
+      res.send(result)
+    });
+
+    // get all user data
+    app.get('/user', verifyJWT, async(req,res) =>{
+      const users = await userCollection.find({}).toArray();
+      res.send(users)
+    });
+
+    // remove a user
+    app.delete('/user/:id', async(req, res) =>{
       const id = req.params.id;
       const query = {_id: ObjectId(id)}
-      const result = playerCollection.findOne(query);
+      const result = userCollection.deleteOne(query);
       res.send(result)
+    });
+
+    // make a user admin
+    app.put('/user/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      const filter = {email: email}
+      const updatedDoc = {
+        $set:{
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+
+    // put a user into the database
+    app.put('/user/:email', async(req, res) =>{
+      const email = req.params.email;
+      const body = req.body;
+      const filter = {email: email}
+      const options = {upsert: true}
+      const updatedDoc = {
+        $set: body
+      }
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+      const result = await userCollection.updateOne(filter, updatedDoc, options)
+      res.send({result, token})
+    });
+
+    // get all the product info
+    app.get("/product", async (req, res) => {
+      const products = await productCollection.find({}).toArray();
+      res.send(products);
     });
 
     // add a player (post method)
